7dc7dac85cb9ac05c1adabe41072032499d7db62
Devialize <888@onlinepbx.ru>
2020-02-20 12:22:30 +0500

    testGetClassNumber

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,7 @@
 public class MainClass {
-    public int getLocalNumber(){
-        int a =14;
-        return a;
+    private int class_number = 20;
+    public int getClassNumber(){
+        return class_number;
     }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass{
     @Test
-    public void testGetLocalNumber(){
-        Assert.assertTrue("getLocalNumber != 14", this.getLocalNumber()==14);
+    public void testGetClassNumber(){
+        Assert.assertTrue("getClassNumber < 45", this.getClassNumber()>45);
     }
 }
